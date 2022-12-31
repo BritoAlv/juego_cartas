0e7da7cd444154b050c61115cbbdadda4d6be9f9
BritoAlv <gomzalezbrito@gmail.com>
2022-12-30 19:21:29 -0500

    re-add computer-player

---
+++ ClassLibrary/Players/Computer_Player.cs
@@ -0,0 +1,39 @@
+namespace Poker;
+/// <summary>
+/// This represent a basic computer player who knows by default how to Bet.
+/// </summary>
+public class Basic_Computer_Player : Player
+{
+    public Basic_Computer_Player(string id, int dinero) : base(id, dinero)
+    {
+    }
+    public override int realizar_apuesta(IGlobal_Contexto contexto)
+    {
+        var mayor_dinero = contexto.PlayerManager.Get_Active_Players(2).Select(x => contexto.Ronda_Contexto.Apuestas.Get_Dinero_Apostado(x)).Max();
+        int apuesta = this.Dinero/10 + 1;
+        if(Hand.rank.Id == "Una Pareja") // has pair.
+        {
+            apuesta =  this.Dinero;
+        }
+
+        if (Hand.rank.Priority >= 3)
+        {
+            if (mayor_dinero == 0)
+            {
+                return this.Dinero;
+            }
+            apuesta = Math.Min(mayor_dinero, this.Dinero);
+        }
+
+        if (apuesta == 0)
+        {
+            return 1;
+        }
+        return apuesta;
+    }
+
+    public override IDecision parse_decision(IGlobal_Contexto contexto)
+    {
+        return new Apostar(this);
+    }
+}
